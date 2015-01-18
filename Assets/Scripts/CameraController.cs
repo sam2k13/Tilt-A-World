72712f7c81d935896b1124c3feed942fc329c362
sam2k13 <sam2k13@gmail.com>
2015-01-17 19:17:44 -0600

    Uploaded Unity Project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject marble;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = marble.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/MarbleController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarbleController : MonoBehaviour {
+	public float maxSpeed;
+	public float initialSpeed;
+	private float currentSpeed;
+	public float sideForce;
+	// Use this for initialization
+	void Start () {
+		currentSpeed = initialSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentSpeed < maxSpeed) {
+			//currentSpeed += .1f * Time.deltaTime;		
+			rigidbody.AddForce(new Vector3(0,0,currentSpeed));
+		} 
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rigidbody.AddForce(new Vector3(-sideForce,0,0));		
+			
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rigidbody.AddForce(new Vector3(sideForce,0,0));		
+			
+		}
+	
+	}
+}
