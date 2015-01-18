62a0635ad9b3f0c879439e54a1d31f6b0b802280
sam2k13 <sam2k13@gmail.com>
2015-01-17 19:47:41 -0600

    Added ScoreScript

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+	public GameObject marble;
+	private int score;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score = (int)marble.gameObject.transform.position.z;
+		Debug.Log (score);
+	}
+}
