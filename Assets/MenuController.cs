a4abda78cf90bc3cdf0faa87da63759343d7f7a0
sam2k13 <sam2k13@gmail.com>
2015-01-18 00:23:03 -0600

    Added Main Menu

---
+++ Assets/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+	//public float tiltAngle;
+	public float tiltSpeed;
+	public Image playButton;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playButton.rectTransform.Rotate (new Vector3 (0, 0, tiltSpeed * Time.deltaTime));
+	
+	}
+	public void loadGame(){
+		Application.LoadLevel (1);
+
+
+	}
+}
